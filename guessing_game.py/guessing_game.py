38d38d2e3bc177d4de97e51ef27288f20460d16c
User319183 <88944590+User319183@users.noreply.github.com>
2021-10-29 17:34:44 -0400

    Update guessing_game.py

--- guessing_game.py/guessing_game.py
+++ guessing_game.py/guessing_game.py
@@ -4,34 +4,64 @@ import math
 from random import randint
 from random import *
 
-number1 = (randint(10,30))
-
-
-print("Welcome to the guessing game.")
-
-print("----------------------------")
-
-print("In this game, you must choose a number between 10-30.")
-
-print("----------------------------")
-
-print("BEGIN!")
-
-print("----------------------------")
-
-
-max1 = int(input("Please Choose a number. ")) # the number must be between the number given at `number1`. The default is 10-30.
-
-answer = number1
 
 
 
-print("----------------------------")
+print(
+    """Welcome to the guessing game."""
+
+"""\n----------------------------"""
 
 
-if answer == max1:
-    print('You Win the game!')
-else:
-    print("You lost! The answer was", answer)
-print("----------------------------")
-input("Press enter to exit")
+"""\nIn this game, you must choose a number between 10-30."""
+
+"""\n----------------------------"""
+
+"""BEGIN!"""
+
+
+"""----------------------------"""
+)
+
+user_wins = 0
+computer_wins = 0
+
+while True: #creates an endless loop 
+    
+
+
+
+    number1 = (randint(1,10))
+
+
+
+    answer = number1
+
+
+    print()
+
+    max1 = int(input("Please Choose a number. ")) # the number must be between the number given at `number1`. The default is 10-30.
+
+
+
+
+
+    print("----------------------------")
+
+
+    if answer == max1:
+        print('You Win the game!')
+
+        user_wins+=1
+        print("You have "+str(user_wins)+" wins")
+
+        print("The computer has "+str(computer_wins)+" win(s)")
+        print("\n----------------------------\n")
+
+
+    else:
+        print("You lost! The answer was", answer)
+
+        computer_wins+=1
+        print("The computer has "+str(computer_wins)+" win(s)")
+        print("\n----------------------------\n")
